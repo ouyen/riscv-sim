36e881445c0471d41a82e8c0610a5272451c1032
ouyen <ouyen@users.noreply.github.com>
2022-08-06 01:30:34 +0800

    CPU.h ok without predict

--- CPU.h
+++ CPU.h
@@ -1,6 +1,7 @@
 #ifndef _CPU_H
 #define _CPU_H
 
+#include <bitset>
 #include "DRAM.h"
 #include "MMU.h"
 
@@ -60,26 +61,76 @@ enum OPCODE {
     UJ = 0x6f,
 };
 
-class CPU
-{
-private:
+class CPU {
+   private:
     /* data */
-public:
-    CPU(MemoryMangerUnit* _mmu,uint32_t _pc,uint32_t stack_point);
+   public:
+    CPU(MemoryMangerUnit* _mmu, uint32_t _pc, uint32_t stack_point);
     ~CPU();
     MemoryMangerUnit* MMU;
-    uint32_t PC;
-    uint32_t reg[32];
-    bool exit_flag=false;
+    uint64_t PC;
+    uint64_t reg[32];
+    // uint32_t reg_lock[32];
+    bitset<32> reg_lock = 0;
+    bool exit_flag = false;
+    uint64_t cycle_count = 0;
+    uint64_t branch_predict();
+
     void run();
 
-    void Fetch();
-    void Decode();
-    void Execute();
-    void Memory();
-    void Writeback();
+    void IF();
+    void ID();
+    void EX();
+    void MEM();
+    void WB();
+
+    void ecall();
+
+    bool stop_flag = false;
+    uint64_t extender(int32_t imm, uint8_t len);
+
+    struct IFID {
+        bool stop = true;
+        // uint32_t stall=0;
+        uint32_t instruction = 0;
+        uint64_t pc = 0;
+    } ifid_new, ifid_old;
+
+    struct IDEX {
+        bool stop = true;
+        REG_NAME rd = ZERO, rs1 = ZERO, rs2 = ZERO;
+        uint8_t opcode = 0, funct3 = 0;
+        uint8_t fun7 = 0;
+        // uint32_t stall=0;
+        int32_t imm = 0;
+        uint64_t pc = 0;
+        uint64_t rs1_reg = 0, rs2_reg = 0;
+    } idex_new, idex_old;
+
+    struct EXMEM {
+        bool stop = true;
+        uint64_t pc = 0;
+        uint64_t alu = 0;
+        uint64_t address = 0;
+        REG_NAME rd = ZERO;
+
+        bool Ctrl_M_MemWrite = false;
+        bool Ctrl_M_MemRead = false;
+
+        bool Ctrl_WB_RegWrite = false;
+        bool Ctrl_WB_MemtoReg = false;
+
+    } exmem_new, exmem_old;
+
+    struct MEMWB {
+        bool stop = true;
+        uint64_t mem_out = 0;
+        uint64_t alu_out = 0;
+
+        bool Ctrl_WB_RegWrite = false;
+        bool Ctrl_WB_MemtoReg = false;
+
+    } memwb_new, memwb_old;
 };
-
-
 
 #endif
