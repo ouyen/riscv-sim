09950efee9fc815f45a4837116f5b58382d4a6c5
ouyen <ouyen@users.noreply.github.com>
2022-08-07 16:26:21 +0800

    ID switch ok

--- CPU.cpp
+++ CPU.cpp
@@ -85,87 +85,98 @@ void CPU::ID() {
     }
 
     idex_new.pc = ifid_old.pc;
+    idex_new.bubble = false;
 
-    // idex_new.stop = false;
     uint32_t inst = ifid_old.instruction;
     idex_new.opcode = inst & 0x7f;
     inst >>= 7;
-    idex_new.rd = (REG_NAME)((inst & 0x1f));
+    REG_NAME rd = (REG_NAME)((inst & 0x1f));
     inst >>= 5;
     idex_new.funct3 = (inst & 0b111);
     inst >>= 3;
-    idex_new.rs1 = (REG_NAME)(inst & 0x1f);
+    REG_NAME rs1 = (REG_NAME)(inst & 0x1f);
     inst >>= 5;
-    idex_new.rs2 = (REG_NAME)(inst & 0x1f);
+    REG_NAME rs2 = (REG_NAME)(inst & 0x1f);
     inst >>= 5;
     idex_new.funct7 = inst;
 
     inst = ifid_old.instruction;  // for imm later
     // tmp use
-    REG_NAME rs1 = idex_new.rs1;
-    REG_NAME rs2 = idex_new.rs2;
-    REG_NAME rd = idex_new.rd;
-    bool lock = false;
     uint32_t imm = 0;
 
+    idex_new.Ctrl_M_MemRead = false;
+    idex_new.Ctrl_M_MemWrite = false;
+    idex_new.Ctrl_WB = NOT_WRITE;
+
     switch ((OPCODE)idex_new.opcode) {
-        // deal with imm , reg_val , add lock , check lock
+        // deal with imm 
+        // set (rs1,rs2,rd) to ZERO if dont use
+        //  idex_new.Ctrl_M_MemRead = ?;
+        //  idex_new.Ctrl_M_MemWrite = ?;
+        //  idex_new.Ctrl_WB = ?;
         case R:
-            // lock = check_lock(rs1) | check_lock(rs2);
-            // reg_lock[rd] = ifid_old.pc;
-            idex_new.rs1_reg = reg[rs1];
-            idex_new.rs2_reg = reg[rs2];
-            idex_new.Ctrl_WB=WB_ALU;
+            idex_new.Ctrl_WB = WB_ALU;
             break;
         case I_LB:
-        imm = (inst >> 20);
-            idex_new.rs1_reg = reg[rs1];
-            idex_new.Ctrl_M_MemRead=true;
-            idex_new.Ctrl_WB=WB_MEM;
+            imm = (inst >> 20);
+            rs2=ZERO;
+            idex_new.Ctrl_M_MemRead = true;
+            idex_new.Ctrl_WB = WB_MEM;
+            break;
         case I_ADDI:
-
         case I_ADDIW:
         case I_JALR:
             imm = (inst >> 20);
-            idex_new.rs1_reg = reg[rs1];
-            // lock = check_lock(rs1);
-            // reg_lock[rd] = ifid_old.pc;
+            rs2=ZERO;
+            idex_new.Ctrl_WB=WB_ALU;
             break;
         case I_ECALL:
-            lock = check_lock_ecall();
+            rs1=A0;
+            rs2=A7;
+            rd=ZERO;
             break;
         case S:
             imm = ((inst >> 7) & 0x1f) | ((inst >> 20) & 0xfe0);
-            lock = check_lock(rs1) | check_lock(rs2);
-            idex_new.rs1_reg = reg[rs1];
-            idex_new.rs2_reg = reg[rs2];
+            rd=ZERO;
+            idex_new.Ctrl_M_MemWrite=true;
             break;
         case SB:
-            lock = check_lock(rs1) | check_lock(rs2);
-            idex_new.rs1_reg = reg[rs1];
-            idex_new.rs2_reg = reg[rs2];
             imm = ((inst >> 7) & 0x1e) | ((inst >> 20) & 0x7e0) |
                   (((inst >> 7) & 0x1) << 11) | ((inst >> 31) << 12);
+            rd=ZERO;
             break;
         case U_AUIPC:
         case U_LUI:
-            reg_lock[rd] = ifid_old.pc;
+            rs1=ZERO;
+            rs2=ZERO;
             imm = (inst & 0xfffff000);
             break;
         case UJ:
-            reg_lock[rd] = ifid_old.pc;
             imm = ((inst >> 31) << 20) | (((inst >> 21) & 0x100) << 1) |
                   ((inst >> 20) & 0x1) << 11 | (inst >> 12 & 0xff) << 12;
+            rs1=ZERO;
+            rs2=ZERO;
             break;
         default:
             error("ID ERROR: opcode %x not found\n", idex_new.opcode);
             break;
     }
-    idex_new.bubble = lock;
-    reg_lock[0] = 0;
+    // idex_new.bubble = lock;
+    // reg_lock[0] = 0;
     idex_new.imm = imm;
-    if(idex_old.Ctrl_WB==WB_WRITE_REG_FROM::WB_MEM and idex_old.rd!=ZERO and (idex_old.rd==idex_new.rs1 or idex_old.rd==idex_new.rs2)){
+    idex_new.rs1 = rs1;
+    idex_new.rs2 = rs2;
+    idex_new.rd = rd;
+    idex_new.rs1_reg = this->reg[rs1];
+    idex_new.rs2_reg = this->reg[rs2];
+
+    if (idex_old.Ctrl_WB == WB_WRITE_REG_FROM::WB_MEM and
+        idex_old.rd != ZERO and
+        (idex_old.rd == idex_new.rs1 or idex_old.rd == idex_new.rs2)) {
         // TODO: pipline stall
+        // which means keep fetch (let pc=ifid_old.pc), ex NOP
+        this->PC = ifid_old.pc;
+        idex_new.bubble = true;
     }
     return;
 }
@@ -177,18 +188,17 @@ void CPU::EX() {
     }
     exmem_new.bubble = false;
     exmem_new.pc = idex_old.pc;
-    exmem_new.rd=idex_old.rd;
+    exmem_new.rd = idex_old.rd;
 
     uint64_t r1 = idex_old.rs1_reg;
     uint64_t r2 = idex_old.rs2_reg;
     // TODO: data hazards flag
     // 数据前递 data forward
 
-
-
     switch ((OPCODE)idex_old.opcode) {
         case R:
-            exmem_new.alu_out=ALU_R(r1,r2,idex_old.funct3<<8|idex_old.funct7);
+            exmem_new.alu_out =
+                ALU_R(r1, r2, idex_old.funct3 << 8 | idex_old.funct7);
             break;
         case I_03:
 
